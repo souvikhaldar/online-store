5f389a772f88bd864e5f286b8cf1b44055dc4ec2
Souvik Haldar <souvik.haldar@kartbites.com>
2019-02-18 01:01:34 +0530

    serving example client.html at /client

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,6 +18,9 @@ func main() {
 	router.HandleFunc("/purchase", product.Purchase).Methods("GET")
 	router.HandleFunc("/longlat", locationtracker.LongLatHandler).Methods("POST")
 	router.HandleFunc("/ws", locationtracker.WsHandler)
+	router.HandleFunc("/client", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "../../client.html")
+	})
 	go locationtracker.Echo()
 	log.Fatal(http.ListenAndServe(":8192", router))
 }
